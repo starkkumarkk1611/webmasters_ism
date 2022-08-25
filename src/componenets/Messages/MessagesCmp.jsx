7e150748401bf57479a06a67ed399e5a439c34d1
Mejar Kumar <Mejarkumar2003@gmail.com>
2022-08-25 11:13:33 +0530

    initialization

---
+++ src/componenets/Messages/MessagesCmp.jsx
@@ -0,0 +1,41 @@
+import "./Messages.css";
+function MessagesCmp() {
+  return (
+    <>
+      <div className="messages-section">
+        <h2 className="title">
+          A repository of key messages from notable personalities and
+          dignitaries.
+        </h2>
+        <div className="container-message">
+          <div className="left-part">
+            <iframe
+              src="https://www.youtube.com/embed/Tj_y2N4NkSE"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className="right-part">
+            <h3>Message from the Chairman</h3>
+            <br />
+            <p>
+              The transformation of Indian education dates back to it being the
+              pioneer in the 800 BCs where the undivided India had global
+              institutions, whereas the deterioration began with the ages of
+              modernization and industrialization.
+              <br />
+              <br />
+              Prof. Anil D. Sahasrabudhe
+              <br />
+              Chairman
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MessagesCmp;
--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import NavBar from "../componenets/headers/NavBar"
 import Footer from "../componenets/footer/Footer"
-import Messages_cmp from "../componenets/Messages/Messages_cmp"
+import MessagesCmp from "../componenets/Messages/MessagesCmp"
 
 export default function Messages() {
     return (
         <>
             <NavBar />
-            <Messages_cmp />
+            <MessagesCmp />
             <Footer />
         </>
     )
